21e8ea66b9d82a42146069de1eff815b38c0f31e
christopher4lis <christopher4lis@gmail.com>
2021-09-17 17:32:41 -0400

    Episode 5 - Browser Resizing

--- main.js
+++ main.js
@@ -270,3 +270,10 @@ document.querySelector('#viewWorkBtn').addEventListener('click', (e) => {
     delay: 2
   })
 })
+
+addEventListener('resize', () => {
+  camera.aspect = innerWidth / innerHeight
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(innerWidth, innerHeight)
+})
